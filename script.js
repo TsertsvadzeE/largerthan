73684eacaf96d49db10f2b3c77f369d28e956d49
TsertsvadzeE <eliso.tsertsvadze99@gmail.com>
2024-07-09 22:26:26 +0400

    navbar responsive

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.getElementById("navbar-toggle").addEventListener("click", function () {
   document.getElementById("navbar-toggle").style.display = "none";
   document.getElementById("navbar").style.flexDirection = "column";
 });
+
+window.addEventListener("resize", function () {
+  if (window.matchMedia("(min-width: 801px)").matches) {
+    document.getElementById("navbar").style.flexDirection = "row";
+  }
+});
