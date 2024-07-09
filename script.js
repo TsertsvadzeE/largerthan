554fd23524b1a548c9782c6d17ba9027c67c0e9c
TsertsvadzeE <eliso.tsertsvadze99@gmail.com>
2024-07-09 18:52:59 +0400

    navbar change

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById("navbar-toggle").addEventListener("click", function () {
   document.getElementById("navbar-list").style.display = "flex";
+  document.getElementById("navbar-toggle").style.display = "none";
+  document.getElementById("navbar").style.flexDirection = "column";
 });
